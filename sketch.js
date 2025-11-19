c4e8b0c04cf04fe5fc3cf69636bcf76b96815948
geometabolism-jpg <geometabolism@gmail.com>
2025-11-18 20:13:24 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,33 +1,29 @@
 let myFont;
-let canvas;
 
 function preload() {
-  myFont = loadFont('Inter-Medium.ttf');
+  myFont = loadFont('Inter-Medium.ttf'); // root folder
 }
 
 function setup() {
-  resizeResponsiveCanvas();
-  canvas.parent(document.body);
-  textFont(myFont);
+  createCanvas(1200, 500, WEBGL);
+  textFont(myFont);        // Use the loaded Inter font
   textAlign(CENTER, CENTER);
   textSize(8);
-  ortho(-width/2, width/2, -height/2, height/2, 0, 1000); // switch to orthographic
 }
 
 function draw() {
   background(0);
 
-  let scaleFactor = width / 1200; // scale based on width
-  let rectWidth = 100 * scaleFactor;
-  let rectHeight = 200 * scaleFactor;
-  let rectDepth = 0.05 * scaleFactor;
-  let depthSpacing = 10 * scaleFactor;
+  let rectWidth = 100;
+  let rectHeight = 200;
+  let rectDepth = 0.05;   // very thin
+  let depthSpacing = 10;
   let numRects = 45;
 
   rotateY(frameCount * 0.008);
 
   stroke(255);
-  strokeWeight(0.5 * scaleFactor);
+  strokeWeight(0.5); // thin edge lines
   fill(0);
 
   for (let i = 0; i < numRects; i++) {
@@ -38,47 +34,23 @@ function draw() {
     fill(255);
     noStroke();
 
+    // Front of the first card
     if (i === 0) {
       push();
-      translate(0, 0, rectDepth / 2 + 0.01 * scaleFactor);
+      translate(0, 0, rectDepth / 2 + 0.01);
       text("ACTOR", 0, 0);
       pop();
     }
 
+    // Back of the last card
     if (i === numRects - 1) {
       push();
-      rotateY(PI);
-      translate(0, 0, rectDepth / 2 + 0.01 * scaleFactor);
+      rotateY(PI); // rotate 180° to face the camera
+      translate(0, 0, rectDepth / 2 + 0.01);
       text("ACTOR", 0, 0);
       pop();
     }
 
     pop();
   }
-}
-
-function resizeResponsiveCanvas() {
-  const maxWidth = windowWidth * 0.95;
-  const maxHeight = windowHeight * 0.8;
-  const ratio = 1200 / 600;
-
-  let canvasWidth = maxWidth;
-  let canvasHeight = canvasWidth / ratio;
-
-  if (canvasHeight > maxHeight) {
-    canvasHeight = maxHeight;
-    canvasWidth = canvasHeight * ratio;
-  }
-
-  if (!canvas) {
-    canvas = createCanvas(canvasWidth, canvasHeight, WEBGL);
-  } else {
-    resizeCanvas(canvasWidth, canvasHeight);
-  }
-
-  ortho(-canvasWidth/2, canvasWidth/2, -canvasHeight/2, canvasHeight/2, 0, 1000);
-}
-
-function windowResized() {
-  resizeResponsiveCanvas();
 }
